91fd7bc4b43d55777ba4209f99be82bac6893ca0
C9bota <16rd050@ms.dendai.ac.jp>
2024-02-10 15:58:46 +0900

    ALLOWED_HOSTの値を修正

--- src/qumcum-api/qumcum_web/qumcum_web/settings.py
+++ src/qumcum-api/qumcum_web/qumcum_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,8 @@ SECRET_KEY = 'django-insecure-9!t&)6ddb7f9j8x((%&6i@wocot)o3j^i7yk#qf8l-yl_rpm^6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+# 環境変数からALLOWED_HOSTSを取得し、カンマで区切られた文字列として指定
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
