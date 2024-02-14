a19f368fba7f3703592b882d72bf6f540ece7671
MagiMagikarpp <147556513+MagiMagikarpp@users.noreply.github.com>
2024-02-15 08:44:28 +0900

    Update urls.py

--- src/qumcum-api/qumcum_web/qumcum_web/urls.py
+++ src/qumcum-api/qumcum_web/qumcum_web/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from qumcum_app.views import index, item_list, completed_purchase, purchase_status
+from qumcum_app.views import index, item_list, completed_purchace, purchase_status
 
 urlpatterns = [
     path('admin/', admin.site.urls),
